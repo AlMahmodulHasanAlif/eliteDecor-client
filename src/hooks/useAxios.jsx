e7ab00244d1b01e309ecc654d1bb695fb07a9764
Al Mahmodul Hasan Alif <almahmodulhasanalif@gmail.com>
2025-12-22 18:07:47 +0600

    jwt added

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,10 +1,51 @@
 import axios from "axios";
-import React from "react";
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router";
 
-const axiosSecure = axios.create({ baseURL: "http://localhost:3000" });
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+});
 
-const useAxios = () => {
+const useAxiosSecure = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+ 
+    const reqInterceptor = axiosSecure.interceptors.request.use(
+      async (config) => {
+        if (user) {
+          const token = await user.getIdToken(); 
+          config.headers.authorization = `Bearer ${token}`; 
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
+
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const statusCode = error.response?.status;
+
+        if (statusCode === 401 || statusCode === 403) {
+          await logOut();
+          navigate("/login");
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, logOut, navigate]);
+
   return axiosSecure;
 };
 
-export default useAxios;
+export default useAxiosSecure;
+
